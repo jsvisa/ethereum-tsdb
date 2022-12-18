45e62d781aa3f47a7bba5fba7993398d3c0565ef
Delweng <delweng@gmail.com>
2022-12-18 22:15:02 +0800

    schema/evm: add item_id
    Signed-off-by: Delweng <delweng@gmail.com>

--- schema/evm.sql
+++ schema/evm.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS {{ chain }}.blocks (
     blkhash                 CHAR(66) NOT NULL,  -- The block hash
     parent_hash             CHAR(66) NOT NULL,  -- The parent block hash
     nonce                   TEXT,               -- The hash of the generated proof-of-work
-    sha3_uncles             CHAR(66) NOT NULL,  -- SHA3 of the uncles data in the block
+    sha3_uncles             CHAR(66),           -- SHA3 of the uncles data in the block
     logs_bloom              TEXT,               -- The bloom filter for the logs of the block
     txs_root                CHAR(66) NOT NULL,  -- The root of the transaction trie of the block
     state_root              CHAR(66) NOT NULL,  -- The root of the final state trie of the block
@@ -30,12 +30,16 @@ CREATE TABLE IF NOT EXISTS {{ chain }}.blocks (
     uncle_count             SMALLINT DEFAULT 0,
     uncle0_hash             CHAR(66) DEFAULT NULL,
     uncle1_hash             CHAR(66) DEFAULT NULL,
+    item_id                 TEXT,
     created_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (blknum, block_timestamp)
+    PRIMARY KEY (item_id, block_timestamp)
 );
+CREATE INDEX IF NOT EXISTS {{ chain }}_blocks_blknum_idx ON {{ chain }}.blocks(blknum);
 SELECT create_hypertable('{{ chain }}.blocks', 'block_timestamp');
+ALTER TABLE {{ chain }}.blocks SET(timescaledb.compress, timescaledb.compress_segmentby = 'blknum', timescaledb.compress_orderby = 'item_id, block_timestamp asc');
+SELECT add_compression_policy('{{ chain }}.blocks', INTERVAL '30 days');
 
 CREATE TABLE IF NOT EXISTS {{ chain }}.txs (
     id                          BIGSERIAL,
@@ -62,16 +66,48 @@ CREATE TABLE IF NOT EXISTS {{ chain }}.txs (
     receipt_status              INTEGER,    -- Either 1 (success) or 0 (failure) (post Byzantium), null before Byzantium
     receipt_effective_gas_price NUMERIC,    -- The actual value per gas deducted from the senders account. Replacement of gas_price after EIP-1559
     receipt_log_count           BIGINT,     -- The number of logs in this transaction
+    item_id                     TEXT,
     created_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (txhash, block_timestamp)
+    PRIMARY KEY (item_id, block_timestamp)
 );
-SELECT create_hypertable('{{ chain }}.txs', 'block_timestamp');
+CREATE INDEX IF NOT EXISTS {{ chain }}_txs_txhash_idx ON {{ chain }}.txs(txhash);
 CREATE INDEX IF NOT EXISTS {{ chain }}_txs_blknum_idx ON {{ chain }}.txs(blknum);
 CREATE INDEX IF NOT EXISTS {{ chain }}_txs_from_addr_st_idx ON {{ chain }}.txs(from_address, block_timestamp);
 CREATE INDEX IF NOT EXISTS {{ chain }}_txs_to_addr_st_idx ON {{ chain }}.txs(to_address, block_timestamp);
 CREATE INDEX IF NOT EXISTS {{ chain }}_txs_contract_addr_st_idx ON {{ chain }}.txs(receipt_contract_address, block_timestamp) WHERE receipt_contract_address is not null;
+SELECT create_hypertable('{{ chain }}.txs', 'block_timestamp');
+ALTER TABLE {{ chain }}.txs SET(timescaledb.compress, timescaledb.compress_segmentby = 'blknum', timescaledb.compress_orderby = 'item_id, block_timestamp asc');
+SELECT add_compression_policy('{{ chain }}.txs', INTERVAL '30 days');
+
+CREATE TABLE IF NOT EXISTS {{ chain }}.txpools (
+    id                          BIGSERIAL,
+    block_timestamp             TIMESTAMP,  -- The current timestamp
+    blknum                      BIGINT,     -- The block number of current blknum
+    txhash                      CHAR(66) NOT NULL,       -- Hash of the transaction
+    txpos                       BIGINT DEFAULT NULL,     -- Integer of the transactions index position in the block
+    nonce                       BIGINT NOT NULL,     -- The number of transactions made by the sender prior to this one
+    from_address                CHAR(42) NOT NULL,       -- Address of the sender
+    to_address                  CHAR(42),   -- Address of the receiver. null when its a contract creation transaction
+    value                       NUMERIC,    -- Value transferred in Wei
+    gas                         BIGINT,     -- Gas provided by the sender
+    gas_price                   NUMERIC,    -- Gas price provided by the sender in Wei
+    input                       TEXT,       -- The data sent along with the transaction
+    max_fee_per_gas             BIGINT,     -- Total fee that covers both base and priority fees
+    max_priority_fee_per_gas    BIGINT,     -- Fee given to miners to incentivize them to include the transaction
+    tx_type                     INTEGER,    -- Transaction type. 0 before London; 0, 2 after London
+    pool_type                   TEXT,       -- available: pending, queued
+    item_id                     TEXT,
+    created_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (item_id, block_timestamp)
+);
+CREATE INDEX IF NOT EXISTS {{ chain }}_txpools_from_addr_st_idx ON {{ chain }}.txpools(from_address, block_timestamp);
+CREATE INDEX IF NOT EXISTS {{ chain }}_txpools_to_addr_st_idx ON {{ chain }}.txpools(to_address, block_timestamp);
+SELECT create_hypertable('{{ chain }}.txpools', 'block_timestamp');
+SELECT add_retention_policy('{{ chain }}.txpools', INTERVAL '3 days');
 
 CREATE TABLE IF NOT EXISTS {{ chain }}.traces (
     id                          BIGSERIAL,
@@ -95,16 +131,19 @@ CREATE TABLE IF NOT EXISTS {{ chain }}.traces (
     trace_address               TEXT,               -- Comma separated list of trace address in call tree
     error                       TEXT,               -- Error if message call failed
     status                      INTEGER,            -- The status of the tx, either 1 (success) or 0 (failure) (post Byzantium)
+    item_id                     TEXT,
     created_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (txhash, trace_address, block_timestamp)
+    PRIMARY KEY (item_id, block_timestamp)
 );
-SELECT create_hypertable('{{ chain }}.traces', 'block_timestamp');
 CREATE INDEX IF NOT EXISTS {{ chain }}_traces_blknum_idx ON {{ chain }}.traces(blknum);
+CREATE INDEX IF NOT EXISTS {{ chain }}_traces_txhash_idx ON {{ chain }}.traces(txhash);
 CREATE INDEX IF NOT EXISTS {{ chain }}_traces_from_addr_st_idx ON {{ chain }}.traces(from_address, block_timestamp);
 CREATE INDEX IF NOT EXISTS {{ chain }}_traces_to_addr_st_idx ON {{ chain }}.traces(to_address, block_timestamp);
-
+SELECT create_hypertable('{{ chain }}.traces', 'block_timestamp');
+ALTER TABLE {{ chain }}.traces SET(timescaledb.compress, timescaledb.compress_segmentby = 'blknum', timescaledb.compress_orderby = 'item_id, block_timestamp asc');
+SELECT add_compression_policy('{{ chain }}.traces', INTERVAL '30 days');
 
 -- extract via Geth's json-rpc eth_getTransactionReceipt
 -- about the column `topics`:
@@ -124,14 +163,18 @@ CREATE TABLE IF NOT EXISTS {{ chain }}.logs (
     topics                      TEXT,       -- Indexed log arguments (0 to 4 32-byte hex strings).
     data                        TEXT,       -- Contains one or more 32 Bytes non-indexed arguments of the log
     topics_0                    TEXT,       -- The first topic, aka the Event signature hash
+    item_id                     TEXT,
     created_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (txhash, logpos, block_timestamp)
+    PRIMARY KEY (item_id, block_timestamp)
 );
-SELECT create_hypertable('{{ chain }}.logs', 'block_timestamp');
 CREATE INDEX IF NOT EXISTS {{ chain }}_logs_blknum_idx ON {{ chain }}.logs(blknum);
+CREATE INDEX IF NOT EXISTS {{ chain }}_logs_txhash_idx ON {{ chain }}.logs(txhash);
 CREATE INDEX IF NOT EXISTS {{ chain }}_logs_address_st_idx ON {{ chain }}.logs(address, block_timestamp);
+SELECT create_hypertable('{{ chain }}.logs', 'block_timestamp');
+ALTER TABLE {{ chain }}.logs SET(timescaledb.compress, timescaledb.compress_segmentby = 'blknum', timescaledb.compress_orderby = 'item_id, block_timestamp asc');
+SELECT add_compression_policy('{{ chain }}.logs', INTERVAL '30 days');
 
 CREATE TABLE IF NOT EXISTS {{ chain }}.contracts (
     id                          BIGSERIAL,
@@ -150,16 +193,16 @@ CREATE TABLE IF NOT EXISTS {{ chain }}.contracts (
     func_sighashes              TEXT,       -- 4-byte function signature hashes
     is_erc20                    BOOLEAN,    -- Whether this contract is an ERC20 contract
     is_erc721                   BOOLEAN,    -- Whether this contract is an ERC721 contract
-    is_proxy                    BOOLEAN,    -- Whether this contract is EIP1947/EIP897 or other proxy
-    upstream                    TEXT,       -- The upstream address of this token
+    item_id                     TEXT,
     created_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (address, txhash, block_timestamp)
+    PRIMARY KEY (item_id, block_timestamp)
 );
-SELECT create_hypertable('{{ chain }}.contracts', 'block_timestamp');
 CREATE INDEX IF NOT EXISTS {{ chain }}_contracts_txhash_idx ON {{ chain }}.contracts(txhash);
 CREATE INDEX IF NOT EXISTS {{ chain }}_contracts_creater_idx ON {{ chain }}.contracts(creater);
+CREATE INDEX IF NOT EXISTS {{ chain }}_contracts_address_idx ON {{ chain }}.contracts(address);
+SELECT create_hypertable('{{ chain }}.contracts', 'block_timestamp');
 
 CREATE TABLE IF NOT EXISTS {{ chain }}.tokens (
     id                          BIGSERIAL,
@@ -200,18 +243,21 @@ CREATE TABLE IF NOT EXISTS {{ chain }}.token_xfers (
     from_address                CHAR(42) NOT NULL,       -- Address of the sender
     to_address                  CHAR(42) NOT NULL,       -- Address of the receiver
     value                       NUMERIC,     -- Amount of tokens transferred (ERC20) / id of the token transferred (ERC721)
-    name                        TEXT,
-    symbol                      TEXT,
-    decimals                    INTEGER,    -- The number of decimals the token uses
+    name                        TEXT,       -- Token name
+    symbol                      TEXT,       -- Token symol
+    decimals                    BIGINT,     -- Token decimals
+    item_id                     TEXT,
     created_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (txhash, logpos, block_timestamp)
+    PRIMARY KEY (item_id, block_timestamp)
 );
-SELECT create_hypertable('{{ chain }}.token_xfers', 'block_timestamp');
 CREATE INDEX IF NOT EXISTS {{ chain }}_token_xfers_token_addr_st_idx ON {{ chain }}.token_xfers(token_address, block_timestamp);
 CREATE INDEX IF NOT EXISTS {{ chain }}_token_xfers_from_addr_st_idx ON {{ chain }}.token_xfers(from_address, block_timestamp);
 CREATE INDEX IF NOT EXISTS {{ chain }}_token_xfers_to_addr_st_idx ON {{ chain }}.token_xfers(to_address, block_timestamp);
+SELECT create_hypertable('{{ chain }}.token_xfers', 'block_timestamp');
+ALTER TABLE {{ chain }}.token_xfers SET(timescaledb.compress, timescaledb.compress_segmentby = 'blknum', timescaledb.compress_orderby = 'item_id, block_timestamp asc');
+SELECT add_compression_policy('{{ chain }}.token_xfers', INTERVAL '30 days');
 
 CREATE TABLE IF NOT EXISTS {{ chain }}.erc721_xfers (
     id                          BIGSERIAL,
@@ -228,17 +274,20 @@ CREATE TABLE IF NOT EXISTS {{ chain }}.erc721_xfers (
     token_id                    NUMERIC,    -- Token ID
     name                        TEXT,
     symbol                      TEXT,
+    item_id                     TEXT,
     created_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (txhash, logpos, block_timestamp)
+    PRIMARY KEY (item_id, block_timestamp)
 );
-SELECT create_hypertable('{{ chain }}.erc721_xfers', 'block_timestamp');
 -- the number of token id transactions under the token address is too sparse in the long term, using
 -- the token address, token id, and _st as the index is much more efficient than _st, token address, and token id as the index
 CREATE INDEX IF NOT EXISTS {{ chain }}_erc721_xfers_token_addr_id_st_idx ON {{ chain }}.erc721_xfers(token_address, token_id, block_timestamp);
 CREATE INDEX IF NOT EXISTS {{ chain }}_erc721_xfers_from_addr_st_idx ON {{ chain }}.erc721_xfers(from_address, block_timestamp);
 CREATE INDEX IF NOT EXISTS {{ chain }}_erc721_xfers_to_addr_st_idx ON {{ chain }}.erc721_xfers(to_address, block_timestamp);
+SELECT create_hypertable('{{ chain }}.erc721_xfers', 'block_timestamp');
+ALTER TABLE {{ chain }}.erc721_xfers SET(timescaledb.compress, timescaledb.compress_segmentby = 'blknum', timescaledb.compress_orderby = 'item_id, block_timestamp asc');
+SELECT add_compression_policy('{{ chain }}.erc721_xfers', INTERVAL '30 days');
 
 CREATE TABLE IF NOT EXISTS {{ chain }}.erc1155_xfers (
     id                          BIGSERIAL,
@@ -260,12 +309,17 @@ CREATE TABLE IF NOT EXISTS {{ chain }}.erc1155_xfers (
     xfer_type                   VARCHAR(20),-- TransferSingle, TransferBatch
     name                        TEXT,
     symbol                      TEXT,
+    item_id                     TEXT,
     created_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at                  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (txhash, logpos, id_pos, block_timestamp)
+    PRIMARY KEY (item_id, block_timestamp)
 );
-SELECT create_hypertable('{{ chain }}.erc1155_xfers', 'block_timestamp');
+-- the number of token id transactions under the token address is too sparse in the long term, using
+-- the token address, token id, and _st as the index is much more efficient than _st, token address, and token id as the index
 CREATE INDEX IF NOT EXISTS {{ chain }}_erc1155_xfers_token_addr_id_st_idx ON {{ chain }}.erc1155_xfers(token_address, token_id, block_timestamp);
 CREATE INDEX IF NOT EXISTS {{ chain }}_erc1155_xfers_from_addr_st_idx ON {{ chain }}.erc1155_xfers(from_address, block_timestamp);
 CREATE INDEX IF NOT EXISTS {{ chain }}_erc1155_xfers_to_addr_st_idx ON {{ chain }}.erc1155_xfers(to_address, block_timestamp);
+SELECT create_hypertable('{{ chain }}.erc1155_xfers', 'block_timestamp');
+ALTER TABLE {{ chain }}.erc1155_xfers SET(timescaledb.compress, timescaledb.compress_segmentby = 'blknum', timescaledb.compress_orderby = 'item_id, block_timestamp asc');
+SELECT add_compression_policy('{{ chain }}.erc1155_xfers', INTERVAL '30 days');
