d20565184c70c78d81efcada2ca71df558a0fb0b
jsvisa <delweng@gmail.com>
2023-03-20 18:03:28 +0800

    *: add a simple script to generate postgres schema
    Signed-off-by: jsvisa <delweng@gmail.com>

---
+++ generate_chain_schema.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from jinja2 import Template
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Generate blockchain schema",
+    )
+    parser.add_argument("-c", "--chain", default="ethereum", help="Blockchain name")
+    parser.add_argument(
+        "--template-file", default="schema/evm.sql", help="Template file"
+    )
+    parser.add_argument(
+        "--timescale-db",
+        action="store_true",
+        help="Database support TimescaleDB extension",
+    )
+    parser.add_argument(
+        "--create-address-index",
+        action="store_true",
+        help="Create {from,to,token}_address related index",
+    )
+
+    args = parser.parse_args()
+
+    with open(args.template_file) as fr:
+        temp = Template(fr.read())
+    result = temp.render(
+        chain=args.chain,
+        is_timescale_db=args.timescale_db,
+        create_address_index=args.create_address_index,
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
