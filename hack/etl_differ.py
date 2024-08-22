fb68a5f0bf6cdb4f3fe4eb8b9eca7f8adea6bd7d
jsvisa <delweng@gmail.com>
2024-08-22 01:35:00 +0000

    hack/etl-differ: register psycopg wait callback

--- hack/etl_differ.py
+++ hack/etl_differ.py
@@ -11,6 +11,14 @@ from jinja2 import Template
 from datetime import timedelta
 from sqlalchemy import create_engine, text
 
+import psycopg2
+import psycopg2.extensions
+import psycopg2.extras
+
+# set wait timeout https://github.com/psycopg/psycopg2/issues/333#issuecomment-543016895
+psycopg2.extensions.set_wait_callback(psycopg2.extras.wait_select)
+
+
 BLOCK_SQL = Template(
     """
 WITH time_range(st, et) AS (
